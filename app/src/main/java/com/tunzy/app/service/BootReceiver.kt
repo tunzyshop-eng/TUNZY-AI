f7f04451a4a975bdb9d1e030a9b75df5cca0ccdd
tunzyshop-eng <tunzyshop@gmail.com>
2026-06-29 08:26:15 +0100

    Create app/src/main/java/com/tunzy/app/service/BootReceiver.kt

---
+++ app/src/main/java/com/tunzy/app/service/BootReceiver.kt
@@ -0,0 +1,15 @@
+package com.tunzy.app.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            context.startForegroundService(
+                Intent(context, WakeWordService::class.java)
+            )
+        }
+    }
+}
